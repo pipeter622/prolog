cf0b80c2f0d0015c09c07961accf4c6d2e2d6dd1
pipeter622 <71790609+pipeter622@users.noreply.github.com>
2020-11-10 00:04:13 -0300

    Update tareaa.pl

--- tareaa.pl
+++ tareaa.pl
@@ -1,3 +1,7 @@
+%mañana sera sabado solo puede ser verdad si alberto lo dice
+%hoy es el tercer dia que miento siempre sera metira por lo que no puede ser ni miercoles, viernes o sabado
+%hola soy alberto sera mentira si lo dice carlos o luis
+
 hermano(carlos).
 hermano(alberto).
 hermano(luis).
@@ -14,7 +18,6 @@ frase('Hoy es el tercer dia que miento').
 frase('Hola, soy alberto').
 frase('Mañana será sabado').
 
-
 posibleV(alberto,'Hola, soy alberto',jueves).
 posibleV(alberto,'Hola, soy alberto',sabado).
 posibleV(alberto,'Hola, soy alberto',domingo).
@@ -23,22 +26,25 @@ posibleV(alberto,'Mañana será sabado',viernes).
 miente_verdad(carlos,alberto,miercoles).
 miente_verdad(carlos,luis,viernes).
 
-miente(alberto,lunes).
-miente(alberto,martes).
-miente(alberto, miercoles).
+miente(alberto,lunes, "mañana es sabado").
+miente(alberto,martes,"mañana es sabado").
+miente(alberto, miercoles,"mañana es sabado").
+miente(alberto,lunes,"hoy es el tercer dia que miento").
+miente(alberto,martes,"hoy es el tercer dia que miento").
 
-miente(carlos,miercoles).
-miente(carlos,jueves).
-miente(carlos,viernes).
+miente(carlos,miercoles,"hoy es el tercer dia que miento").
+miente(carlos,jueves,"hoy es el tercer dia que miento").
+miente(carlos,viernes,'Hola, soy alberto').
 
-miente(luis,viernes).
-miente(luis,sabado).
-miente(luis,domingo).
+miente(luis,viernes,"hoy es el tercer dia que miento").
+miente(luis,sabado,"hoy es el tercer dia que miento").
+miente(luis,domingo,'Hola, soy alberto').
 
 verdad(alberto,jueves).
 verdad(alberto,viernes).
 verdad(alberto,sabado).
 verdad(alberto,domingo).
+verdad(alberto,viernes).
 
 verdad(carlos,lunes).
 verdad(carlos,martes).
@@ -50,4 +56,4 @@ verdad(luis,martes).
 verdad(luis,miercoles).
 verdad(luis,jueves).
 
-respuesta(X,Y,A,B,Z):-verdad(Y,X),miente_verdad(A,B,X),miente(B,X),posibleV(Y,Z,X).
+respuesta(X,Y,A,B,Z,S,D):-verdad(Y,X),miente_verdad(A,D,X),miente(D,X,S),posibleV(Y,Z,X),miente(A,X,B).
